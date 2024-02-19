ae1f35ed0cd6af7a25565cf1c9c53383e15292fb
Daksh Jain <dakshbjain@gmail.com>
2024-02-19 12:55:05 -0800

    Add ability to load a game board.

--- classes/board.py
+++ classes/board.py
@@ -1,5 +1,8 @@
 """A chess board and its interactions."""
 
+import csv
+import immutabledict
+
 from classes import constants
 from classes import pieces
 
@@ -11,6 +14,21 @@ _BasePiece = pieces.BasePiece
 
 # Constants.
 _EMPTY_STRING = ' '
+# Maps the text for each piece to their constructors.
+_PIECE_MAPPING = immutabledict.immutabledict({
+    'Pawn': pieces.Pawn,
+    'Rook': pieces.Rook,
+    'Knight': pieces.Knight,
+    'Bishop': pieces.Bishop,
+    'Queen': pieces.Queen,
+    'King': pieces.King,
+})
+
+# Maps the text for each color to the enum.
+_COLOR_MAPPING = immutabledict.immutabledict({
+    'Black': _Color.BLACK,
+    'White': _Color.WHITE,
+})
 
 
 class BoardSquare:
@@ -20,7 +38,7 @@ class BoardSquare:
         validate_coordinates(x, y)
         self._x = x
         self._y = y
-        self._color = square_color(self._x, self._y)
+        self._color = _Color.BLACK if (x + y) % 2 else _Color.WHITE
         # Ensure that the piece is on the correct square.
         if piece:
             piece.set_x(self._x)
@@ -43,19 +61,20 @@ class BoardSquare:
         if self._piece:
             return self._piece.get_icon()
         elif self.color() == _Color.WHITE:
-            return 'W'
+            return '■'
         elif self.color() == _Color.BLACK:
-            return 'B'
+            return '□'
 
 
 class Board:
     """A basic chess board."""
 
-    def __init__(self) -> None:
+    def __init__(self, load: bool = False) -> None:
         """Initializes a basic chess board."""
         self._board: Sequence[Sequence[BoardSquare]] = [[BoardSquare(i, j) for i in range(constants.BOARD_SIZE)]
                                                         for j in range(constants.BOARD_SIZE)]
-        print(self._board)
+        if load:
+            self.load_board()
 
     def __str__(self) -> str:
         board_str = ''
@@ -64,6 +83,31 @@ class Board:
                 board_str += self._board[i][j].get_display() + _EMPTY_STRING
             board_str += '\n'
         return board_str
+
+    def load_board(self, path='./games/default.csv') -> None:
+        """Loads pieces onto a board.
+
+        The game is loaded from a CSV filepath. The file must be in the following
+        format:
+            x, y, piece, color
+        Example:
+            0, 1, Pawn, Black
+
+        Parameters:
+        -----------
+        path : str
+            File path for the CSV file to read from. Defaults to a starting game position
+                for the board.
+        """
+        with open(path, newline='\n') as csvfile:
+            for row in csv.reader(csvfile):
+                x = int(row[0])
+                y = int(row[1])
+                color = _COLOR_MAPPING.get(row[3], None)
+                piece_constructor = _PIECE_MAPPING.get(row[2], None)
+                if piece_constructor and color:
+                    piece = piece_constructor(x=x, y=y, color=color)
+                    self._board[x][y].set_piece(piece)
 
     def set_piece(self, piece: _BasePiece) -> None:
         validate_coordinates(piece.x(), piece.y())
@@ -74,9 +118,3 @@ def validate_coordinates(x: int, y: int) -> bool:
     if x < 0 or x >= constants.BOARD_SIZE or y < 0 or y >= constants.BOARD_SIZE:
         raise ValueError(
             f'Invalid x ({x}) or y ({y}) coorindate for board size of {constants.BOARD_SIZE}.')
-
-
-def square_color(x: int, y: int) -> _Color:
-    if (x + y) % 2:
-        return _Color.WHITE
-    return _Color.BLACK
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 """File to run and play the chess game."""
 
 from classes.board import Board
-from classes.pieces import King
-from classes.constants import Color
 
 
 # Basic game testing.
-board = Board()
-king = King(color=Color.BLACK)
-board.set_piece(king)
+board = Board(load=True)
 print(board)
